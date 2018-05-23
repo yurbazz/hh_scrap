f83eaba636cf74f33172aef48e6d1128b5170495
yurbazz <yuriko.ru@gmail.com>
2018-05-23 21:47:49 +0800

    Added url to new job titles

--- include/job_titles.php
+++ include/job_titles.php
@@ -10,7 +10,9 @@ include('db.php');
 echo "<a href=\"..\index.php\">На главную страницу</a>";
 
 $form_footer = "<button type=\"submit\">Обработать</button>";
-$sql = "SELECT id, title FROM `titles` WHERE type IS NULL";
+$sql = "SELECT t.id, t.title, ji.url FROM titles t \n".
+  "JOIN jobs_info ji ON t.id = ji.title_id\n".
+  "WHERE t.type IS NULL";
 
 $result = mysqli_query($connection, $sql);
 
@@ -19,6 +21,7 @@ if ($result->num_rows <> 0) {
   while ($row = mysqli_fetch_assoc($result)) {
 		echo "<div class=\"title-block\">".
     "<span class=\"title\">".$row["title"]."</span><br />\n";
+    echo "<a class=\"link-small\" href=\"".$row["url"]."\" target=\"_blank\">".$row["url"]."</a><br />";
 		echo "<input type=\"radio\" name=\"".$row["id"]."\" value=\"1\" id=\"tracked".$row["id"]."\">".
 		"<label for=\"tracked".$row["id"]."\">Отслеживать</label><br />".
 		"<input type=\"radio\" name=\"".$row["id"]."\" value=\"2\" id=\"not-tracked".$row["id"]."\" checked>".
